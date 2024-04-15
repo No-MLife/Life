e4624a34ba34eea9ccb6ce8769ee73388746f92f
Junyong <jypark93@kookmin.ac.kr>
2024-04-15 16:43:25 +0900

    #8 게시글 API 구현
    - POST
    - PUT
    - GET
    - DELETE
    구현 및 테스트 완료

--- M_life/src/main/java/com/m_life/m_life/MLifeApplication.java
+++ M_life/src/main/java/com/m_life/m_life/MLifeApplication.java
@@ -2,8 +2,10 @@ package com.m_life.m_life;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class MLifeApplication {
 
 	public static void main(String[] args) {
---
+++ M_life/src/main/java/com/m_life/m_life/controller/Post/postController.java
@@ -0,0 +1,42 @@
+package com.m_life.m_life.controller.Post;
+
+import com.m_life.m_life.dto.request.PostRequest;
+import com.m_life.m_life.dto.response.PostResponse;
+import com.m_life.m_life.service.PostService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/")
+public class postController {
+
+    private final PostService postService;
+    @PostMapping("/post")
+    public ResponseEntity<String> createPost(@RequestBody PostRequest postRequest){
+        return postService.save(postRequest);
+    }
+
+    @PutMapping("/post/{postid}")
+    public ResponseEntity<String> updatePost(@RequestBody PostRequest postRequest, @PathVariable(name = "postid") Integer id){
+        return postService.update(postRequest, id);
+    }
+
+    @DeleteMapping("/post/{postid}")
+    public ResponseEntity<String> deletePost(@PathVariable(name= "postid") Integer id){
+        return postService.delete(id);
+    }
+
+    @GetMapping("/post")
+    public List<PostResponse> getPosts(){
+        return postService.getAllposts();
+    }
+
+    @GetMapping("/post/{postid}")
+    public PostResponse getPost(@PathVariable(name = "postid") Integer id){
+        return postService.getMypost(id);
+    }
+}
