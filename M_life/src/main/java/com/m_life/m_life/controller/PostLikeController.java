b786f6d6d3c01bf367280d64ec597f866078abf8
Junyong <jypark93@kookmin.ac.kr>
2024-04-23 19:47:16 +0900

    #12-like_api
    - postlike 컨트롤러 URI 수정
    posts -> post

--- M_life/src/main/java/com/m_life/m_life/controller/PostLikeController.java
+++ M_life/src/main/java/com/m_life/m_life/controller/PostLikeController.java
@@ -1,39 +1,47 @@
 package com.m_life.m_life.controller;
 
 import com.m_life.m_life.domain.UserAccount;
+import com.m_life.m_life.service.CustomUserDetails;
 import com.m_life.m_life.service.PostLikeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/posts/{postId}/like")
+@RequestMapping("/api/post/{postId}/like")
 @RequiredArgsConstructor
 public class PostLikeController {
     private final PostLikeService postLikeService;
 
 
     @PostMapping
-    public ResponseEntity<Void> likePost(@PathVariable Long postId, @AuthenticationPrincipal UserAccount userAccount) {
+    public ResponseEntity<Void> likePost(@PathVariable(name = "postId") Long postId,
+                                         @AuthenticationPrincipal CustomUserDetails userDetails) {
+        UserAccount userAccount = userDetails.getUserAccount();
         postLikeService.likePost(postId, userAccount);
         return ResponseEntity.ok().build();
     }
 
     @DeleteMapping
-    public ResponseEntity<Void> unlikePost(@PathVariable Long postId, @AuthenticationPrincipal UserAccount userAccount) {
+    public ResponseEntity<Void> unlikePost(@PathVariable(name = "postId") Long postId,
+                                           @AuthenticationPrincipal CustomUserDetails userDetails) {
+        UserAccount userAccount = userDetails.getUserAccount();
         postLikeService.unlikePost(postId, userAccount);
         return ResponseEntity.ok().build();
     }
 
     @GetMapping("/count")
-    public ResponseEntity<Long> getLikeCount(@PathVariable Long postId) {
+    public ResponseEntity<Long> getLikeCount(@PathVariable(name = "postId") Long postId) {
         long likeCount = postLikeService.getLikeCount(postId);
         return ResponseEntity.ok(likeCount);
     }
 
     @GetMapping("/liked")
-    public ResponseEntity<Boolean> isLikedByCurrentUser(@PathVariable Long postId, @AuthenticationPrincipal UserAccount userAccount) {
+    public ResponseEntity<Boolean> isLikedByCurrentUser(@PathVariable(name = "postId") Long postId,
+                                                        @AuthenticationPrincipal CustomUserDetails userDetails) {
+        UserAccount userAccount = userDetails.getUserAccount();
         boolean isLiked = postLikeService.isLikedByUser(postId, userAccount);
         return ResponseEntity.ok(isLiked);
     }
