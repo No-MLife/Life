47bb4b38e757a09ed189f2d96ff4f13412c9cb74
Junyong <jypark93@kookmin.ac.kr>
2024-04-25 15:11:04 +0900

    #main
    - flutter SDK 업데이트

--- flutterworkspace/m_life_app/lib/main.dart
+++ flutterworkspace/m_life_app/lib/main.dart
@@ -21,7 +21,7 @@ class StoragePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: ,
+      body: Text("Hello World"),
     );
   }
 }
