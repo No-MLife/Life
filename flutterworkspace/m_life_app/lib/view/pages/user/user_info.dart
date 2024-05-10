a3eaabd7916efba0205534908d16ad6305a1e6c8
Junyong <jypark93@kookmin.ac.kr>
2024-05-10 23:46:11 +0900

    #31 회원정보
    - 회원정보 페이지 (스프링부트 닉네임 변경 설정 필요)
        - 프로필 이미지 및 설명 어떻게 할 지 생각필요

--- flutterworkspace/m_life_app/lib/view/pages/user/user_info.dart
+++ flutterworkspace/m_life_app/lib/view/pages/user/user_info.dart
@@ -1,29 +1,110 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:m_life_app/controller/user_controller.dart';
-import 'package:m_life_app/view/pages/user/login_page.dart';
 
-import '../../components/custom_header_navi.dart';
+import '../../components/buildBottomNavigationBar.dart';
 
-class UserInfo extends StatelessWidget {
+class UserInfo extends StatefulWidget {
+  @override
+  _ProfileSettingsPageState createState() => _ProfileSettingsPageState();
+}
+
+class _ProfileSettingsPageState extends State<UserInfo> {
+  final UserController _userController = Get.find();
+  TextEditingController _nameController = TextEditingController();
+  TextEditingController _bioController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _nameController.text = _userController.principal.value.nickname ?? '';
+    _bioController.text = _userController.principal.value.memo ?? '';
+  }
+
   @override
   Widget build(BuildContext context) {
-    UserController u = Get.find();
     return Scaffold(
-      appBar: CustomAppBar(
-        title: 'M-Life',
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("프로필 설정"),
+        backgroundColor: Colors.amber[300],
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [Text("회원 번호 "),
-            IconButton(
-              icon: Icon(Icons.arrow_back),
-              onPressed: () => Get.off(() => LoginPage()),
-            )
-          ],
-        ),
+      body: ListView(
+        padding: EdgeInsets.all(16.0),
+        children: [
+          Center(
+            child: GestureDetector(
+              onTap: () {
+                // 프로필 이미지 선택 또는 촬영 로직 추가
+              },
+              child: Container(
+                width: 200,
+                height: 200,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.white70,
+                ),
+                child: Image.asset(
+                  'assets/logo.png',
+                  fit: BoxFit.contain,
+                ),
+              ),
+            ),
+          ),
+          SizedBox(height: 20),
+          TextField(
+            controller: _nameController,
+            decoration: InputDecoration(
+              labelText: '사용자 이름',
+              labelStyle: TextStyle(color: Colors.black54),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10.0),
+                borderSide: BorderSide(color: Colors.amber[300]!),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10.0),
+                borderSide: BorderSide(color: Colors.amber[300]!),
+              ),
+            ),
+          ),
+          SizedBox(height: 16),
+          TextField(
+            controller: _bioController,
+            maxLines: 3,
+            decoration: InputDecoration(
+              labelText: '소개',
+              labelStyle: TextStyle(color: Colors.black54),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10.0),
+                borderSide: BorderSide(color: Colors.amber[300]!),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10.0),
+                borderSide: BorderSide(color: Colors.amber[300]!),
+              ),
+            ),
+          ),
+          SizedBox(height: 32),
+          ElevatedButton(
+            onPressed: () {
+              // 프로필 설정 저장 로직 추가
+              print("유저 설정 변경 로직 추가");
+            },
+            style: ElevatedButton.styleFrom(
+              foregroundColor: Colors.white,
+              backgroundColor: Colors.amber[500],
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20),
+              ),
+            ),
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 24.0, vertical: 16.0),
+              child: Text('저장하기', style: TextStyle(fontSize: 16)),
+            ),
+          ),
+        ],
       ),
+      bottomNavigationBar: buildBottomNavigationBar(),
     );
   }
 }
