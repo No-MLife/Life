6d6aaaaf9f7508a5596e40ce9e9331d4792a701f
Junyong <jypark93@kookmin.ac.kr>
2024-06-20 09:32:17 +0900

    #84 MSA
    - msa전환(유레카 서버)

---
+++ MSA/discoveryservice/src/test/java/com/m_life/discoveryservice/DiscoveryserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.m_life.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
