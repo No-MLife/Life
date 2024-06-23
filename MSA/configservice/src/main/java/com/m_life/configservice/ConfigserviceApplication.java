a44ae79724964a84b7f6c54aa1ac552dd60fd2c6
Junyong <jypark93@kookmin.ac.kr>
2024-06-24 02:16:42 +0900

    #88 MSA-CONFIG
    - Config server add

---
+++ MSA/configservice/src/main/java/com/m_life/configservice/ConfigserviceApplication.java
@@ -0,0 +1,15 @@
+package com.m_life.configservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigserviceApplication.class, args);
+    }
+
+}
---
+++ MSA/configservice/src/test/java/com/m_life/configservice/ConfigserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.m_life.configservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
